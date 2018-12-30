34e0718f5d4353463973e415622d9e781a7ff1ec
Daniel Martins <daniel.martins@descomplica.com.br>
2018-12-30 13:56:04 -0200

    Day 6

--- 2018/06/main.go
+++ 2018/06/main.go
@@ -1,11 +1,160 @@
 package main
 
 import (
+	"bufio"
+	"flag"
+	"fmt"
+	"log"
 	"math"
+	"os"
+	"strconv"
+	"strings"
 )
 
 type point struct {
 	x, y int
+}
+
+var input = flag.String("input", "input", "Puzzle input file")
+
+func main() {
+	flag.Parse()
+
+	points := loadPoints(*input)
+	grid := makeGrid(points)
+	area := calculateArea(grid)
+
+	fmt.Printf("Size of the largest non-infinite area: %d.\n", pointIDWithLargestNonInfiniteArea(area))
+}
+
+func loadPoints(filename string) []point {
+	points := []point{}
+
+	file, err := os.Open(filename)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		pointData := strings.Split(scanner.Text(), ", ")
+
+		x, err := strconv.Atoi(pointData[0])
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		y, err := strconv.Atoi(pointData[1])
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		points = append(points, point{
+			x: x,
+			y: y,
+		})
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+
+	return points
+}
+
+func makeGrid(points []point) [][]int {
+	maxX, maxY := 0, 0
+
+	for _, point := range points {
+		if point.x > maxX {
+			maxX = point.x
+		}
+		if point.y > maxY {
+			maxY = point.y
+		}
+	}
+
+	// Initialize the empty grid
+	grid := make([][]int, maxY+1)
+	for y := range grid {
+		grid[y] = make([]int, maxX+1)
+	}
+
+	// Calculate the closest element for every position
+	for y := range grid {
+		for x := range grid[y] {
+			closestDistance := math.MaxInt64
+			closestPoints := []int{}
+
+			for pi, p := range points {
+				d := manhattanDistance(point{x: x, y: y}, p)
+
+				if d < closestDistance {
+					closestDistance = d
+					closestPoints = []int{pi}
+				} else if d == closestDistance {
+					closestPoints = append(closestPoints, pi)
+				}
+			}
+
+			if len(closestPoints) > 1 {
+				grid[y][x] = -1
+			} else {
+				grid[y][x] = closestPoints[0]
+			}
+		}
+	}
+
+	return grid
+}
+
+func printGrid(grid [][]int) {
+	for x := range grid {
+		line := ""
+		for y := range grid[x] {
+			if grid[x][y] == math.MaxInt64 {
+				line += ". "
+			} else {
+				line += fmt.Sprintf("%+d ", grid[x][y])
+			}
+		}
+		fmt.Println(line)
+	}
+}
+
+func calculateArea(grid [][]int) map[int]int {
+	area := map[int]int{}
+
+	for y := range grid {
+		for x := range grid[y] {
+			pid := grid[y][x]
+
+			if pid == -1 {
+				continue
+			}
+
+			if y == 0 || y == len(grid)-1 || x == 0 || x == len(grid[y])-1 {
+				area[pid] = math.MaxInt64
+			} else if area[pid] != math.MaxInt64 {
+				area[pid]++
+			}
+		}
+	}
+
+	return area
+}
+
+func pointIDWithLargestNonInfiniteArea(area map[int]int) int {
+	id := -1
+
+	for pid := range area {
+		if area[pid] != math.MaxInt64 && (id == -1 || area[pid] > area[id]) {
+			id = pid
+		}
+	}
+
+	return area[id]
 }
 
 func manhattanDistance(p1, p2 point) int {
--- 2018/06/main_test.go
+++ 2018/06/main_test.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"math"
+	"reflect"
 	"testing"
 )
 
@@ -34,5 +36,94 @@ func Test_manhattanDistance(t *testing.T) {
 				t.Errorf("Expected distance to be %d, but was %d", tc.expected, actual)
 			}
 		})
+	}
+}
+
+func Test_loadPoints(t *testing.T) {
+	expected := []point{
+		{
+			x: 1,
+			y: 1,
+		},
+		{
+			x: 1,
+			y: 6,
+		},
+		{
+			x: 8,
+			y: 3,
+		},
+		{
+			x: 3,
+			y: 4,
+		},
+		{
+			x: 5,
+			y: 5,
+		},
+		{
+			x: 8,
+			y: 9,
+		},
+	}
+	actual := loadPoints("input.test")
+
+	if !reflect.DeepEqual(actual, expected) {
+		t.Errorf("Expected %+v to be %+v", actual, expected)
+	}
+}
+
+func Test_makeGrid(t *testing.T) {
+	points := loadPoints("input.test")
+
+	expected := [][]int{
+		{+0, +0, +0, +0, +0, -1, +2, +2, +2},
+		{+0, +0, +0, +0, +0, -1, +2, +2, +2},
+		{+0, +0, +0, +3, +3, +4, +2, +2, +2},
+		{+0, +0, +3, +3, +3, +4, +2, +2, +2},
+		{-1, -1, +3, +3, +3, +4, +4, +2, +2},
+		{+1, +1, -1, +3, +4, +4, +4, +4, +2},
+		{+1, +1, +1, -1, +4, +4, +4, +4, -1},
+		{+1, +1, +1, -1, +4, +4, +4, +5, +5},
+		{+1, +1, +1, -1, +4, +4, +5, +5, +5},
+		{+1, +1, +1, -1, +5, +5, +5, +5, +5},
+	}
+	actual := makeGrid(points)
+
+	if !reflect.DeepEqual(actual, expected) {
+		t.Errorf("Expected grid to be %+v, but was %+v", expected, actual)
+	}
+}
+
+func Test_calculateArea(t *testing.T) {
+	points := loadPoints("input.test")
+	grid := makeGrid(points)
+
+	expected := map[int]int{
+		0: math.MaxInt64,
+		1: math.MaxInt64,
+		2: math.MaxInt64,
+		3: 9,
+		4: 17,
+		5: math.MaxInt64,
+	}
+
+	actual := calculateArea(grid)
+
+	if !reflect.DeepEqual(actual, expected) {
+		t.Errorf("Expected areas to be %+v, but was %+v", expected, actual)
+	}
+}
+
+func Test_pointIDWithLargestNonInfiniteArea(t *testing.T) {
+	points := loadPoints("input.test")
+	grid := makeGrid(points)
+	area := calculateArea(grid)
+
+	expected := 17
+	actual := pointIDWithLargestNonInfiniteArea(area)
+
+	if actual != expected {
+		t.Errorf("Expected largest non-infinite area to be %d, but was %d", expected, actual)
 	}
 }
